a71d6a5aa5456acfbe0fd4b91f31430c7a0811a4
praditya19 <praditya.rendi88@gmail.com>
2021-08-05 13:47:21 +0700

    struktur terbaru

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,7 @@
-const BASE_PATH = "http://localhost:3000/api/";
+const BASE_PATH = "";
 
 const config = {
   api: {
-    tutor: `${BASE_PATH}tutors/list`,
-    learning: `${BASE_PATH}learningsources/list`,
-    students: `${BASE_PATH}students`,
-    question: `${BASE_PATH}questions`,
-    paragraphtypes: `${BASE_PATH}paragraphtypes`,
-    scores: `${BASE_PATH}scores`,
-    studentactions: `${BASE_PATH}studentactions`,
-    topicGuides: `${BASE_PATH}topicGuides`,
-    topics: `${BASE_PATH}topics`,
   },
 };
 export default config;
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import tutor from "./modules/tutor";
-import learning from "./modules/learningSources";
-import students from "./modules/students";
-import diagnostik from "./modules/diagnostik";
-import topics from "./modules/topics";
-import scores from "./modules/scores";
-import studentActions from "./modules/studentActions";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    tutor,
-    learning,
-    students,
-    diagnostik,
-    topics,
-    scores,
-    studentActions
-  },
+  modules: {},
 });
